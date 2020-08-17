339151c586f6cc5dc4d6f7c237b2d05bddbfd54b
ctbarrel <ctbarrel@gmail.com>
2020-08-17 11:02:15 -0600

    fizzbuzz

---
+++ fizzbuzz/fizzbuzz.js
@@ -0,0 +1,13 @@
+for (var fb=1; fb <=100; fb++) {
+    if (Number.isInteger(fb/3)) {
+        if (Number.isInteger(fb/5)) {
+            console.log(`Fizzbuzz`)
+        } else {
+            console.log(`Fizz`)
+        }
+    } else if (Number.isInteger(fb/5)) {
+        console.log(`Buzz`)
+    } else {
+        console.log(fb)
+    }
+}
