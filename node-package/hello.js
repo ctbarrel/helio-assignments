0561111b62abfe675fb9bdf4deb291d0eedd74d6
ctbarrel <ctbarrel@gmail.com>
2020-08-21 10:53:56 -0600

    node package

---
+++ node-package/hello.js
@@ -0,0 +1,7 @@
+// let colors = require('colors/safe')
+
+// function sayHello() {
+//     console.log(colors.america(`Hello World`))
+// }
+
+// module.exports.sayHello = sayHello
---
+++ node-package/index.js
@@ -0,0 +1,4 @@
+// let {sayHello} = require ("./hello")
+
+// sayHello()
+//can destructure objects like arrays, but by name rather than position.
